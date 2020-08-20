aadf1e74d18b60475b5061d726a30e09687ad6e1
Eamon Powell <eamonpowell@outlook.com>
2020-08-20 17:40:34 +1000

    wahoo: Make BoardConfigWahoo.mk override BoardConfig.mk
    Move the BoardConfigWahoo.mk inherit to the end of
    BoardConfig.mk to allow us to override already defined
    properties.
    
    Change-Id: Idf203925985b08bd2430114f014c5c6eeeb69fb9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Allow wahoo config to override others
--include device/google/wahoo/BoardConfigWahoo.mk
-
 TARGET_BOARD_PLATFORM := msm8998
 
 TARGET_ARCH := arm64
@@ -171,3 +168,6 @@ BOARD_VNDK_VERSION := current
 
 # Generate an APEX image for experiment b/119800099.
 DEXPREOPT_GENERATE_APEX_IMAGE := true
+
+# Allow wahoo config to override others
+-include device/google/wahoo/BoardConfigWahoo.mk
